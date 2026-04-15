6c8399e42e306191916d3b33a054a1a088d08839
Aastik Mandil <52568184+Aastik-Mandil@users.noreply.github.com>
2026-04-16 01:29:52 +0530

    Leetcode 18. 4Sum Solved

--- Problem_18(4Sum)/main.cpp
+++ Problem_18(4Sum)/main.cpp
@@ -2,13 +2,14 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         sort(nums.begin(), nums.end());
+        long long tar = target;
 
         set<vector<int>> res;
         int n = nums.size();
         for(int i=0;i<n;i++){
             for(int j=i+1;j<n;j++){
                 int st=j+1,ed=n-1;
-                int total = target - (nums[i] + nums[j]);
+                long long total = tar - nums[i] - nums[j];
                 while(st<ed){
                     if(nums[st]+nums[ed] == total){
                         res.insert({nums[i], nums[j], nums[st], nums[ed]});
