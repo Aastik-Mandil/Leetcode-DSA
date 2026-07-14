5808ca5e021f2a4e10aa2c975cdb6eba9fd05b98
Aastik Mandil <52568184+Aastik-Mandil@users.noreply.github.com>
2026-07-14 09:36:55 +0530

    Leetcode 3895. Count Digit Appearances Solved
    Implement a method to count occurrences of a digit in a list of numbers.

---
+++ Problem_3895(Count-Digit-Appearances)/main.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countDigitOccurrences(vector<int>& nums, int digit) {
+        int ans = 0;
+        for(int &num: nums){
+            while(num > 0){
+                if(num%10 == digit){
+                    ans++;
+                }
+                num /= 10;
+            }
+        }
+        return ans;
+    }
+};
+// PROBLEM:- https://leetcode.com/problems/count-digit-appearances/description/
