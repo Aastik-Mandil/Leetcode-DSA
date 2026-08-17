a4221a53167932ff6a3d4343fb7d4cc0f1a7b3d6
Aastik Mandil <52568184+Aastik-Mandil@users.noreply.github.com>
2026-08-17 11:16:29 +0530

    Leetcode 3847. Find the Score Difference in a Game Solved
    Implement scoreDifference function to calculate score difference based on game rules.

---
+++ Problem_3847(Find-the-Score-Difference-in-a-Game)/main.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int scoreDifference(vector<int>& nums) {
+        int first = 0, second = 0;
+        bool flag = true;
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]%2 != 0){
+                flag = !flag;
+            }
+            if((i+1)%6 == 0){
+                flag = !flag;
+            }
+            if(flag){
+                first += nums[i];
+            }
+            else{
+                second += nums[i];
+            }
+        }
+        return first - second;
+    }
+};
+// PROBLEM:- https://leetcode.com/problems/find-the-score-difference-in-a-game/description/
